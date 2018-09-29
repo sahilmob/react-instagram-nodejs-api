67f19761f15edd235946752fb8ca7f6b07efe9ba
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2018-09-30 00:38:04 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(express.static("img"));
+
+const port = process.env.PORT || 3000;
+
+const images = ["1.jgp", "2.jpg", "3.jpg", "4.jgp", "5.jpg", "6.jpg"];
+
+app.post("/login", (req, res) => {
+	const imgArr = [];
+
+	for (let i = 0; i < 3; i++) {
+		images.sort(function() {
+			return 0.5 - Math.random();
+		});
+		const portString = process.env.PORT ? null : `:${port}`;
+		imgArr.push(`${req.hostname}${portString}/${images[0]}`);
+	}
+	res.json(imgArr);
+});
+
+app.get("/login", (req, res) => {
+	const imgArr = [];
+	for (let i = 0; i < 3; i++) {
+		images.sort(function() {
+			return 0.5 - Math.random();
+		});
+		const portString = process.env.PORT ? null : `:${port}`;
+		imgArr.push(`${req.hostname}${portString}/${images[0]}`);
+	}
+	res.json(imgArr);
+});
+
+app.listen(port, () => {
+	console.log("Server is running on port " + port);
+});
