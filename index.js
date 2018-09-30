0f22bdb98f877a29b625db5161aad299c55e8804
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2018-10-01 01:23:43 +0300

    Fix cors error and add images array

--- index.js
+++ index.js
@@ -1,43 +1,66 @@
 const express = require("express");
-const cors = require('cors')
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const fs = require("fs");
 
 const app = express();
-app.use(cors)
+app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static("img"));
 
 const port = process.env.PORT || 3000;
 
-fs.readdir("./img", (err, filesDir) => {
-	let files = [];
-	filesDir.forEach(file => {
-		files.push(file);
-	});
-});
+let images = [
+	"1",
+	"2",
+	"3",
+	"4",
+	"5",
+	"6",
+	"7",
+	"8",
+	"9",
+	"10",
+	"11",
+	"12",
+	"13",
+	"14",
+	"15",
+	"16",
+	"17",
+	"18",
+	"19",
+	"20",
+	"21",
+	"22",
+	"23",
+	"24",
+	"25",
+	"26",
+	"27",
+	"28",
+	"29"
+];
 
 app.post("/login", (req, res) => {
-	const imgArr = [];
-
-	for (let i = 0; i < 3; i++) {
+	for (let i = 0; i < 12; i++) {
 		images.sort(function() {
 			return 0.5 - Math.random();
 		});
 		const portString = process.env.PORT ? "" : `:${port}`;
-		imgArr.push(`https://${req.hostname}${portString}/${images[0]}`);
+		imgArr.push(`https://${req.hostname}${portString}/${images[0]}.jpg`);
 	}
 	res.json(imgArr);
 });
 
 app.get("/login", (req, res) => {
 	const imgArr = [];
-	for (let i = 0; i < 3; i++) {
+	for (let i = 0; i < 12; i++) {
 		images.sort(function() {
 			return 0.5 - Math.random();
 		});
 		const portString = process.env.PORT ? "" : `:${port}`;
-		imgArr.push(`https://${req.hostname}${portString}/${images[0]}`);
+		imgArr.push(`https://${req.hostname}${portString}/${images[0]}.jpg`);
 	}
 	res.json(imgArr);
 });
